660dbf90715b7b02b35d20a0a995c29ae7e6e0e3
Oliver Dry <olliedry1999@hotmail.co.uk>
2020-06-29 11:53:06 +0100

    Implemented Simple Power Calculating

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,45 @@ namespace Destiny2PowerLevelMax
         public MainWindow()
         {
             InitializeComponent();
+            clear();
+        }
+
+        private void clear()
+        {
+            Helment_tb.Clear();
+            Arms_tb.Clear();
+            Chest_tb.Clear();
+            Legs_tb.Clear();
+            Class_tb.Clear();
+
+            Prim_tb.Clear();
+            Power_tb.Clear();
+            Power_tb.Clear();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                float hel = float.Parse(Helment_tb.Text);
+                float arms = float.Parse(Arms_tb.Text);
+                float Chest = float.Parse(Chest_tb.Text);
+                float Class = float.Parse(Class_tb.Text);
+                float Legs = float.Parse(Legs_tb.Text);
+                float Prim = float.Parse(Prim_tb.Text);
+                float Secondary = float.Parse(Power_tb.Text);
+                float Power = float.Parse(Power_tb.Text);
+
+                float cal = hel + arms + Chest + Class + Legs + Prim + Secondary + Power;
+                float ant = cal / 8;
+
+
+                Score_tb.Text = ant.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
         }
     }
 }
